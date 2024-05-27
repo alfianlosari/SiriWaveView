46ff2b50f027952cc6be739cd0faadee5c815eb6
Alfian Losari <alfianlosari@gmail.com>
2024-05-27 16:40:56 +0700

    Update to use observable

--- Sources/SiriWaveView/Models/SiriWave.swift
+++ Sources/SiriWaveView/Models/SiriWave.swift
@@ -7,60 +7,62 @@
 //
 
 import SwiftUI
+import Observation
 
-class SiriWave: ObservableObject {
+@Observable
+class SiriWave {
     
-    struct Curve: Equatable {
-        
-        var power: Double
-        var A: Double
-        var k: Double
-        var t: Double
-        
-        static func random(withPower power: Double) -> Curve {
-            
-            return Curve(
-                power: power,
-                A: Double.random(in: 0.1...1.0),
-                k: Double.random(in: 0.6...0.9),
-                t: Double.random(in: -1.0...4.0)
-            )
-            
-        }
-        
-    }
-    
-    struct Wave: Equatable {
-        
-        var power: Double
-        var curves: [Curve]
-        var useCurves: Int
-        
-        static func random(withPower power: Double) -> Wave {
-            
-            let numCurves = Int.random(in: 2 ... 4)
-            
-            return Wave(
-                power: power,
-                curves: (0..<4).map { _ in
-                    return Curve.random(withPower: power)
-                },
-                useCurves: numCurves
-            )
-            
-        }
-        
-    }
-    
-    var waves: [Wave]
+    var waves: [Wave] = []
     
     init(numWaves: Int, power: Double) {
-        
-        waves = [Wave]()
-        
+        updateWaves(numWaves: numWaves, power: power)
+    }
+    
+    func updateWaves(numWaves: Int, power: Double) {
         for _ in 0..<numWaves {
             waves.append(.random(withPower: power))
         }
+    }
+    
+}
+
+struct Curve: Equatable {
+    
+    var power: Double
+    var A: Double
+    var k: Double
+    var t: Double
+    
+    static func random(withPower power: Double) -> Curve {
+        
+        return Curve(
+            power: power,
+            A: Double.random(in: 0.1...1.0),
+            k: Double.random(in: 0.6...0.9),
+            t: Double.random(in: -1.0...4.0)
+        )
+        
+    }
+    
+}
+
+struct Wave: Equatable {
+    
+    var power: Double
+    var curves: [Curve]
+    var useCurves: Int
+    
+    static func random(withPower power: Double) -> Wave {
+        
+        let numCurves = Int.random(in: 2 ... 4)
+        
+        return Wave(
+            power: power,
+            curves: (0..<4).map { _ in
+                return Curve.random(withPower: power)
+            },
+            useCurves: numCurves
+        )
         
     }
     
@@ -91,7 +93,7 @@ class SiriWave: ObservableObject {
 // This part is temporary because you cannot create an
 // array of animatable data
 
-extension SiriWave.Wave: Animatable {
+extension Wave: Animatable {
     
     typealias AnimatableData = AnimatablePair<
         AnimatablePair<
